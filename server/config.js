872311c0479d31af28184844146f39dc28b00050
Altronxs <kylextron101098@gmail.com>
2025-11-04 21:31:12 -1000

    update file structure

--- server/config.js
+++ server/config.js
@@ -1,15 +1,22 @@
 require('dotenv').config(); 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+try {
+    const serviceAccount = require("./makfr-hacc-firebase-adminsdk-fbsvc.json");
 
-initializeApp({
-  credential: cert(serviceAccount),
-  databaseURL: "https://makfr-hacc-default-rtdb.firebaseio.com"
-});
+    initializeApp({
+      credential: cert(serviceAccount),
+      databaseURL: "https://makfr-hacc-default-rtdb.firebaseio.com"
+    });
 
-//const analytics = getAnalytics(firebaseApp);
-const db = getFirestore();
-const User = db.collection('Users');
+    //const analytics = getAnalytics(firebaseApp);
+    const db = getFirestore();
+    const User = db.collection('Users');
 
-module.exports = User;
+    module.exports = User;
+} catch (error) {
+    console.error("Firebase service account key file is missing. Please provide 'makfr-hacc-firebase-adminsdk-fbsvc.json'.");
+    process.exit(1);
+}
+
+
